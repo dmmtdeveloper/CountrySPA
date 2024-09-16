637c633bdf846913fc66322390d37b3083722703
David Martínez <dmmtapia.ux@gmail.com>
2024-09-16 13:10:06 -0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/homePage/home-page.component';
+import { AboutPageComponent } from './shared/pages/aboutPage/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 import { HomePageComponent } from './pages/homePage/home-page.component';
 import { AboutPageComponent } from './pages/aboutPage/about-page.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
