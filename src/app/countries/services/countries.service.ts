90ca5d99bcf125e9be1308d44e90d49e56d1af69
unknown <dmmtapia.ux@gmail.com>
2024-09-23 11:11:27 -0300

    Argumentos por URL - Página de País

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -4,19 +4,28 @@ import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
-
-
 export class CountriesService {
-
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
 
+  searchCountryByAlphaCode(code: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError(() => of([]))
-    );
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
   }
 }
