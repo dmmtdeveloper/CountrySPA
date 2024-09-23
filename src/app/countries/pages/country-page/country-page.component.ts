654008e19a5c588c53b2e6bde56ebeb0f7f406b5
unknown <dmmtapia.ux@gmail.com>
2024-09-23 12:19:24 -0300

    Mostrar información del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``,
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -23,12 +26,10 @@ export class CountryPageComponent implements OnInit {
         )
       )
       .subscribe((country) => {
-        if (!country) {
-          return this.router.navigateByUrl('');
-        }
+        if (!country) return this.router.navigateByUrl('');
 
-        console.log('Tenemos un país');
-        return;
+        return (this.country = country);
+        
       });
   }
 }
