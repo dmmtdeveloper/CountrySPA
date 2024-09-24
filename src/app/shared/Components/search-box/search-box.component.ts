d68313ea4181a317786820b4fe3d4b50b07914eb
unknown <dmmtapia.ux@gmail.com>
2024-09-24 09:13:20 -0300

    Debounce - Peticiones cuando el usuario deja de escribir

--- src/app/shared/Components/search-box/search-box.component.ts
+++ src/app/shared/Components/search-box/search-box.component.ts
@@ -1,22 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``,
 })
+export class SearchBoxComponent implements OnInit {
+  private debouncer: Subject<string> = new Subject<string>();
 
-
-export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
 
   //2 Se usa en el componente hijo para declarar el evento que el padre escuchará.
   @Output()
-  public onValue = new EventEmitter<string>()
+  public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
+      this.onDebounce.emit(value)
+    });
+  }
 
   //3 crear un metodo que maneja el valor actual del input
-  emitValue(value:string):void{
-    this.onValue.emit(value)
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
   }
 }
